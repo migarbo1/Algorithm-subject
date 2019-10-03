1e6470d22dba1b2c7cc0c22cf4a999e1d7dbbe61
migarbo1 <migarbo1@inf.upv.es>
2019-10-03 18:01:06 +0200

    canviar els none per declaracions de numpy

--- distance_caller.py
+++ distance_caller.py
@@ -2,12 +2,12 @@
 #! -*- encoding utf8
 
 ## zona de imports
-import numpy as np
+#import numpy as np
 import re
 import sys
 import os
-from levenstein_distance import levenstein_caller
-from damerau_levenstein import damerau_caller
+from levenstein_distance import better_levenstein_distance
+from damerau_levenstein import damerau_levenstein_distance
 ##
 
 # clean text
@@ -15,7 +15,42 @@ clean_re = re.compile('\W+')
 def clean_text(text):
     return clean_re.sub(' ', text)
 #fin clean text
+#funcion que llama para cada palabra del diccionario a la funcion de levenstein
+def caller(file, tollerance,token, mode):
+    file = open(file,"r")
+    text = file.read()
+    file.close()
+    text = clean_text(text)
+    text = text.lower()
+    text = text.split()
+    dictionary = {}
+    for t in text:
+        dictionary[t] = dictionary.get(t,[])
+        if dictionary[t] == []:
+            if mode == 1:
+                dictionary[t] = [better_levenstein_distance(t,token)] ###aqui esta la llamada###
+            else:
+                dictionary[t] = [damerau_levenstein_distance(t,token)] ###aqui esta la llamada###
 
+    #llegados a este punto tenemos un diccionario con clave = palabra y valor = distancia respecto al token
+    #tenemos que pasarlo a un diccionario con clave = distancia y valor y palabras para asi poder recuperar las que tengan distancia <= tollerance
+    wordsPerDist = {}
+    for k in dictionary:
+        v = dictionary[k][0]
+        wordsPerDist[v] = wordsPerDist.get(v,[])
+        if wordsPerDist[v] == []:
+            wordsPerDist[v] = [k]
+        else:
+            wordsPerDist[v] = wordsPerDist.get(v) + [k]
+
+    #sobre el ultimo diccionario, recuperamos aquellas listas de palabras con clave <= tolerancia
+    for tol in range(0, int(tollerance)):
+        res = wordsPerDist.get(tol,[])
+        if not res == []:
+            print("tolerancia: " + str(tol) + "\n" + "palabras: ")
+            print(res)
+            print("\n")
+#fin de la funcion levenstein_caller
 
 #main
 if __name__ == "__main__":
@@ -23,10 +58,7 @@ if __name__ == "__main__":
         f = sys.argv[1]
         k = sys.argv[2]
         to = sys.argv[3]
-        mode = sys.argv[4]
-        if mode == 1:
-            levenstein_caller(f,k,to)
-        else:
-            damerau_caller(f,k,to)
+        m = sys.argv[4]
+        caller(f,k,to,m)
     else:
         syntax()
--- levenstein_distance.py
+++ levenstein_distance.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 #! -*- encoding utf8
 
-## zona de imports
-import numpy as np
-import re
-import sys
-import os
-##
-
 #distancia de levenstein sin ahorro espacial
 def levenstein_distance(word1, word2):
     levMat = np.zeros(dtype=np.int8, shape=(len(word1) + 1,len(word2) + 1))
@@ -28,8 +21,8 @@ def levenstein_distance(word1, word2):
 
 #distancia de levenstein con ahorro de espacio
 def better_levenstein_distance(word1, word2):
-    c_row = [none] * (len(word1) + 1)
-    p_row = [none] * (len(word1) + 1)
+    c_row = [None] * (len(word1) + 1)
+    p_row = [None] * (len(word1) + 1)
     c_row[0] = 0
     for i in range(1, len(word1) + 1):
         c_row[i] = c_row[i - 1] + 1
@@ -42,38 +35,3 @@ def better_levenstein_distance(word1, word2):
                             p_row[i - 1] + (word1[i - 1] != word2[i - 1]))#elemento anteior en x e y mas si son diferentes (sustitucion por el mismo es 0)
     return c_row[len(word1)]
 #fin distancia levenstein con ahorro espacial
-
-
-#funcion que llama para cada palabra del diccionario a la funcion de levenstein
-def levenstein_caller(file, tollerance,token):
-    file = open(file,"r")
-    text = file.read()
-    file.close()
-    text = clean_text(text)
-    text = text.lower()
-    text = text.split()
-    dictionary = {}
-    for t in text:
-        dictionary[t] = dictionary.get(t,[])
-        if dictionary[t] == []:
-            dictionary[t] = [better_levenstein_distance(t,token)] ###aqui esta la llamada###
-
-    #llegados a este punto tenemos un diccionario con clave = palabra y valor = distancia respecto al token
-    #tenemos que pasarlo a un diccionario con clave = distancia y valor y palabras para asi poder recuperar las que tengan distancia <= tollerance
-    wordsPerDist = {}
-    for k in dictionary:
-        v = dictionary[k][0]
-        wordsPerDist[v] = wordsPerDist.get(v,[])
-        if wordsPerDist[v] == []:
-            wordsPerDist[v] = [k]
-        else:
-            wordsPerDist[v] = wordsPerDist.get(v) + [k]
-
-    #sobre el ultimo diccionario, recuperamos aquellas listas de palabras con clave <= tolerancia
-    for tol in range(0, int(tollerance)):
-        res = wordsPerDist.get(tol,[])
-        if not res == []:
-            print("tolerancia: " + str(tol) + "\n" + "palabras: ")
-            print(res)
-            print("\n")
-#fin de la funcion levenstein_caller
