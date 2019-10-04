ea8f29fe3ab24091834864b6e84524cf49ef03dd
migarbo1 <migarbo1@inf.upv.es>
2019-10-04 12:56:28 +0200

    para provar

--- distance_caller.py
+++ distance_caller.py
@@ -24,30 +24,28 @@ def caller(file, tollerance,token, mode):
     text = text.lower()
     text = text.split()
     dictionary = {}
+    wordsPerDist = {}
     for t in text:
         dictionary[t] = dictionary.get(t,[])
         if dictionary[t] == []:
             if mode == 1:
-                dictionary[t] = [better_levenstein_distance(t,token)] ###aqui esta la llamada###
+                r = [better_levenstein_distance(t,token)] ###aqui esta la llamada###
+                if wordsPerDist.get(r,[]) == []:
+                    wordsPerDist[r] = [t]
+                else:
+                    wordsPerDist[r] += [t]
             else:
-                dictionary[t] = [damerau_levenstein_distance(t,token)] ###aqui esta la llamada###
-
-    #llegados a este punto tenemos un diccionario con clave = palabra y valor = distancia respecto al token
-    #tenemos que pasarlo a un diccionario con clave = distancia y valor y palabras para asi poder recuperar las que tengan distancia <= tollerance
-    wordsPerDist = {}
-    for k in dictionary:
-        v = dictionary[k][0]
-        wordsPerDist[v] = wordsPerDist.get(v,[])
-        if wordsPerDist[v] == []:
-            wordsPerDist[v] = [k]
-        else:
-            wordsPerDist[v] = wordsPerDist.get(v) + [k]
+                r = [damerau_levenstein_distance(t,token)] ###aqui esta la llamada###
+                if wordsPerDist.get(r,[]) == []:
+                    wordsPerDist[r] = [t]
+                else:
+                    wordsPerDist[r] += [t]
 
     #sobre el ultimo diccionario, recuperamos aquellas listas de palabras con clave <= tolerancia
     for tol in range(0, int(tollerance) + 1):
         res = wordsPerDist.get(tol,[])
         if not res == []:
-            print("tolerancia: " + str(tol) + "\n" + "palabras: ")
+            print("tolerancia: " + str(tol) + "\n" + "palabras: " + srt(len(res)))
             print(res)
             print("\n")
 #fin de la funcion levenstein_caller
