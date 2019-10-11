65ce621c47c8eb8460704188d4498cde04c3a48b
Gnis97 <gnisms97@gmail.com>
2019-10-11 19:56:18 +0200

    gia levenstein vs trie

---
+++ levenstein_vs_trie.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#! -*- encoding utf8
+
+#trie es una estructura en la que calculas la distancia de lenestein desde el nodo 0 en adelante. para cada nodo debes enfrontar la letra de la transición con la el simbolo actual de la palabra
+#levenstein_vs_trie(a[0],0) = 0 donde 0 es el identificador del nodo
+#levenstein_vs_trie(a[i],0) = i donde i es la posicion de la letra que estas analizando dentro de la palabra
+#levenstein_vs_trie(a[0],X) = depth(X) donde X es un nodo diferente del 0
+#levenstein_vs_trie(a[i],X) = 
+#min(
+#levenstein_vs_trie(a[i - 1], X) + 1
+#levenstein_vs_trie(a[i], parent(X)) + 1
+#levenstein_vs_trie(a[i - 1], parent(X)) + 1 * a[i] != char(X)
+#)
