25a32615790bccde36ffb13d9e701267da6e0528
pilarpiqueres <pilarpiqueres98@gmail.com>
2019-11-29 18:08:09 +0100

    damerau_vs_trie corregit

--- lib/damerau_levenstein_vs_trie.py
+++ lib/damerau_levenstein_vs_trie.py
@@ -1,5 +1,5 @@
 #import
-import numpy
+import numpy as np
 import TRie as trie
  
  #tenim el trie, paraula i tolereancia
@@ -9,41 +9,39 @@ import TRie as trie
  #cas 3 node X 0 lletres = profundidad ATRIBUTO // hem de tornar "la quantitat de voltes que hem inserit" això ve donat per la capa en la que esta i de ahi profundidad
  #cas general node X lletra 1-len(paraula) -> min entre la tipica + lletra actual = node fill // lletra següent = node actual
 
- def damreau_levenstein_vs_trie(trie, word, toler){
- 	node = trie.root()
- 	matrix = np.zeros(dtype=np.int8, shape=(len(trie.array) + 1, len(word) + 1))
- 	cont = 0
- 	casTransp = False;
- 	matrix[node.idi, 0] = 0
+def damerau_levenstein_vs_trie(word, toler):
+ 	
+    node = trie.root()
+    matrix= np.zeros(dtype = np.int8,shape = (len(trie.array)+1, len(word)+1))
+    cont = 0
+    casTransp = False;
+    matrix[node.idi, 0] = 0
+    aux = 3000
 
- 	for l in word{
- 		matrix[node.idi, l] = l
- 	}
-
- 	for n in trie.array{
-		matrix[n.idi, 0] = profundidad
-		for ll in word{
-			fills = n.hijos;
-			for f in fills{
-				if(f.char == ll && word[cont + 1] == n.char){
-					casTransp = True;
-				}
-			}
-			if(node.idi != n.idi){
-				matrix[n.idi, ll] = min(
-						matrix[n.idi, i-1] + 1, #ins
-                		matrix[n.parent().idi, i + 1], #borr
-                		matrix[n.parent().idi, i - 1] + (word[i] != n.char()), #sust
-                		if(casTransp){ matrix[n.parent().idi, i-2] } #canvi lletres
-
-            			)
-			}
-		cont += 1
-		} 	
- 	}
+    for l in word:
+        matrix[node.idi, l] = l
+ 	
+    for n in trie.array:
+        matrix[n.idi, 0] = trie.profundidad
+        for ll in range(word):
+            fills = n.hijos;
+            for f in fills:
+                if(f.char == ll and word[cont + 1] == n.char):
+                    casTransp = True;
+				
+			
+            if(node.idi != n.idi):
+                if(casTransp):
+                   aux = matrix[n.parent().idi, ll-2] #canvi lletres
+                matrix[n.idi, ll] = min(
+                        matrix[n.idi, ll-1] + 1, #ins
+                        matrix[n.parent().idi, ll + 1], #borr
+                        matrix[n.parent().idi, ll - 1] + (word[ll] != n.char()), #sust
+                        aux               
+            			)            
+        cont += 1
 
 
- }
 
 
 
